3713207443d1c7f5ce2b187eead345c94f76d585
Tyler Kendall <tylerchristiankendall@gmail.com>
2022-06-19 21:32:23 -0600

    fortune button finished

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,15 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["You will be sent on a mission to kill puti- i mean uh... sorry forgot you can't be political, you will be sent on a mission to save the world very soon", "One day you will die... oooooh scary", "Someone, someday, will give you a compliment", "Sex is cool but you ever watched funny monkey videos?", "hey ;)"];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
     }
 
 }
